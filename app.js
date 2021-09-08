79cb2b496d7ca78a78706ecb6cdef61847758772
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-09-08 19:52:42 +0800

    подключен cors и morgan

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+
+
+
 const authRoutes = require('./routes/auth')
 const analyticsRoutes = require('./routes/analytics')
 const categoryRoutes = require('./routes/category')
@@ -7,9 +10,10 @@ const orderRoutes = require('./routes/order')
 const positionRoutes = require('./routes/position')
 const app = express()
 
+app.use(require('morgan')('dev'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
-
+app.use(require('cors')())
 app.use('/api/auth', authRoutes)
 app.use('/api/analytics', analyticsRoutes)
 app.use('/api/category', categoryRoutes)
