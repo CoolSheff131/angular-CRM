8e6c02e4e45ec3feac013e9669ff1ee58e575f78
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-09-08 16:36:05 +0800

    Добавлен контроллер auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,10 @@
 const express = require('express')
+const controller = require('../controllers/auth')
 const router = express.Router()
 
-router.get('/login',(req,res)=>{
-    res.status(200).json({
-        login: true
-    })
-})
+//localhost:5000/api/auth/login
+router.get('/login',controller.login)
 
+//localhost:5000/api/auth/register
+router.get('/register',controller.register)
 module.exports = router
