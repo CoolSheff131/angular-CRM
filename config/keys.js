3c7606c650b36c86cc490d99c3e66ee611a17c36
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-09-09 17:07:24 +0800

    подключен jwt добавлена регистрация

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://CoolSheff:Sasha131@cluster0.1bd2s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
+    mongoURI: 'mongodb+srv://CoolSheff:Sasha131@cluster0.1bd2s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    jwt: 'dev-jwt'
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,37 @@
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const User = require('../models/User')
+const keys = require('../config/keys')
 
 
+module.exports.login = async function(req,res) {
+    const candidate = await User.findOne({email: req.body.email})
 
-module.exports.login = function(req,res) {
-    res.status(200).json({
-        login: {
-            email: req.body.email,
-            password: req.body.password
+    if(candidate){
+        //Проверить пароль
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
+        if(passwordResult){
+            //Генерация токена, пароли совпали
+
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id
+            }, keys.jwt, {expiresIn: 60 * 60})
+            res.status(200).json({
+                token: `Bearer ${token}`
+            })
+        }else{
+            //Неправильный пароль
+            res.status(401).json({
+                message: 'Пароль не правильный'
+            })
         }
-    })
+    }else{
+        //Пользователь не существует
+        res.status(404).json({
+            message: 'Пользователь не найден'
+        })
+    }
 }
 
 module.exports.register = async function(req,res){
@@ -34,7 +56,7 @@ module.exports.register = async function(req,res){
             await user.save()
             res.status(201).json(user)
         }catch(e){
-            
+
             //Обработать ошибку
         }
         
